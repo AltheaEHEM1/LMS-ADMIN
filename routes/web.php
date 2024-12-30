6078ebc26565ecf83809abc8ab2bc91bffd8f59f
Althea <altheaamorjasis@iskolarngbayan.pup.edu.ph>
2024-12-30 19:05:31 +0800

    updated1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,57 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+//////////////////////////////////////////////////////////
+//if you want to run the log in of employee
 Route::get('/', function () {
-    return view('welcome');
+    return view('login_employee');
+});
+
+Route::get('/DASHBORDandingpage_employee', function () {
+    return view('DASHBORDandingpage_employee'); 
+});
+
+Route::get('/EMPLOYEE', function () {
+    return view('EMPLOYEE'); 
+});
+
+Route::get('/RESERVATION', function () {
+    return view('RESERVATION'); 
+});
+
+Route::get('/LIBRARIAN', function () {
+    return view('LIBRARIAN'); 
+});
+
+Route::get('/CATALOGER', function () {
+    return view('CATALOGER'); 
+});
+
+Route::get('/MEMBERS', function () {
+    return view('MEMBERS'); 
+});
+
+Route::get('/CIRCULATION', function () {
+    return view('CIRCULATION'); 
+});
+
+Route::get('/CIRCULATION_REPORTS', function () {
+    return view('CIRCULATION_REPORTS'); 
+});
+
+Route::get('/MEMBER_REPORTS', function () {
+    return view('MEMBER_REPORTS'); 
+});
+
+Route::get('/OVERDUE_REPORTS', function () {
+    return view('OVERDUE_REPORTS'); 
+});
+
+Route::get('/CATALOG', function () {
+    return view('CATALOG'); 
+});
+
+Route::get('/CATALOG_REPORTS', function () {
+    return view('CATALOG_REPORTS'); 
 });
